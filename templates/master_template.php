cdf335359dd6d7d3f27f541ab086906d24ae0e34
Kristi Henno <hennokri@gmail.com>
2014-09-15 15:06:07 +0300

    Rename welcome controller to posts

--- templates/master_template.php
+++ templates/master_template.php
@@ -44,7 +44,9 @@
 		</div>
 		<div class="navbar-collapse collapse">
 			<ul class="nav navbar-nav">
-				<li class="active"><a href="#">Home</a></li>
+				<li class="active"><a href="<?=BASE_URL?>">Posts</a></li>
+                <li><a href="<?=BASE_URL?>tags">Tags</a></li>
+                <li><a href="<?=BASE_URL?>users">Users</a></li>
 				<li><a href="#about">About</a></li>
 				<li><a href="#contact">Contact</a></li>
 				<li class="dropdown">
