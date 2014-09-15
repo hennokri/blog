e8a11bf048b83a90121837f4eae409202ab051dc
Kristi Henno <hennokri@gmail.com>
2014-09-15 13:43:49 +0300

    Rename welcome controller to posts

--- views/posts/posts_index.php
+++ views/posts/posts_index.php
@@ -1,16 +1,12 @@
-{<?foreach( $posts as $post ):?>}
-    <div class="span8">
-        <h1><a href=<?=BASE_URL?>posts/view/<?=$post['post_id']?></a></h1>
-        <p><?$post['andmebaasiväljanimi']?></p>
-        <div>
-            <span class="badge badge-success">Posted 2012-08-02 20:47:04</span><div class="pull-right"><span class="label">alice</span> <span class="label">story</span> <span class="label">blog</span> <span class="label">personal</span></div>
-        </div>
-        <hr>
-        <h1><?$post['andmebaasiväljanimi']?></h1>
-        <p><?$post['andmebaasiväljanimi']?></p>
-        <div>
-            <span class="badge badge-success">Posted 2012-08-02 20:47:04</span><div class="pull-right"><span class="label">alice</span> <span class="label">story</span> <span class="label">blog</span> <span class="label">personal</span></div>
-        </div>
-        <hr>
+<?foreach( $posts as $post ):?>
+    <?foreach ($tags[$post['post_id']]as $tag):?>
+        <a href="#"><span class="label" style="background-color: #5bc0de"><?=$tag?></span></a>
+    <?endforeach?>
+    <h1><a href="<?=BASE_URL?>posts/view/<?=$posts['posts_id']?>"><?=$post['post_subject']?></a></h1>
+    <p><?=$post['post_text']?></p>
+    <div>
+        <span class="badge">Posted 2012-08-02 20:47:04</span><div class="pull-right"><span class="label label-default">alice</span> <span class="label label-primary">story</span> <span class="label label-success">blog</span> <span class="label label-info">personal</span> <span class="label label-warning">Warning</span>
+            <span class="label label-danger">Danger</span></div>
     </div>
-    {<?endforeach?>}
+
+<?endforeach?>
